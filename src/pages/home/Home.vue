5c0fbcdabc87ce707f6b851ba4d6d4230490812a
liulunshuai <1422691123.qq.com>
2019-05-31 15:21:40 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">hello I`m Home.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+    .home{
+        font-size: 30px;
+    }
+</style>
