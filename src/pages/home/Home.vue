4fedcd49c68e40400300c906c21e74056a97d2ff
liulunshuai <1422691123.qq.com>
2019-06-01 15:48:34 +0800

    static change

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div class="wrapper">
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/homeHeader'
 import HomeSwiper from './components/homeSwiper'
+import HomeIcons from './components/homeIcons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
---
+++ src/pages/home/components/homeIcons.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="wrapper">
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+    <div class="icon-panel">
+        <div class="img-panel">
+            <img class="image" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png">
+        </div>
+        <p class="icon-desc">景点门票</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    overflow: hidden
+    .icon-panel
+      position: relative
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      overflow: hidden
+      float: left
+      .img-panel
+        height: 70%
+        width: 70%
+        margin: 0 auto
+        .image
+          width: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        bottom: 0
+        height: 30%
+        width: 100%
+        text-align: center
+        font-size: 0.45rem
+
+</style>
